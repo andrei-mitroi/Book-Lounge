f22ee8db6288449e872609544ab88f3dae63add1
mitroi.cristian.andrei@gmail.com <mitroi.cristian.andrei@gmail.com>
2023-03-06 16:44:42 +0200

    - initial setup

---
+++ src/main/java/com/example/booklounge/BookLoungeApplication.java
@@ -0,0 +1,14 @@
+package com.example.booklounge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLoungeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLoungeApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/example/booklounge/BookLoungeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booklounge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookLoungeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
