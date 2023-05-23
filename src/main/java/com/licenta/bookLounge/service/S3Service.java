bb8d56577d654fddf5fed262a17fa3c179d02b13
mitroi.cristian.andrei@gmail.com <mitroi.cristian.andrei@gmail.com>
2023-05-23 12:06:37 +0300

    - refactored code

--- src/main/java/com/licenta/bookLounge/service/S3Service.java
+++ src/main/java/com/licenta/bookLounge/service/S3Service.java
@@ -2,28 +2,30 @@ package com.licenta.bookLounge.service;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
 import software.amazon.awssdk.services.s3.model.GetUrlRequest;
 
 @Service
 public class S3Service {
 
-    @Value("${spring.aws.accessKey}")
-    private String accessKey;
-    @Value("${spring.aws.secretKey}")
-    private String secretKey;
     @Value("${spring.aws.region}")
     private String region;
     @Value("${spring.aws.bucketName}")
     private String bucketName;
 
     public String generatePresignedUrl(String fileName) {
-        S3Client s3Client = S3Client.builder()
-                .region(Region.of(region))
-                .credentialsProvider(() -> AwsBasicCredentials.create(accessKey, secretKey))
-                .build();
+
+        S3ClientBuilder clientBuilder = S3Client.builder()
+                .region(Region.of(region));
+
+        AwsCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+        clientBuilder.credentialsProvider(credentialsProvider);
+
+        S3Client s3Client = clientBuilder.build();
 
         GetUrlRequest getUrlRequest = GetUrlRequest.builder()
                 .bucket(bucketName)
