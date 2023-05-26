190f5fecf414c2c6181b2d5371557b058604ddf4
mitroi.cristian.andrei@gmail.com <mitroi.cristian.andrei@gmail.com>
2023-05-26 14:52:28 +0300

    - implemented working download file service to Amazon S3

--- src/main/java/com/licenta/bookLounge/service/S3Service.java
+++ src/main/java/com/licenta/bookLounge/service/S3Service.java
@@ -1,17 +1,14 @@
 package com.licenta.bookLounge.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.GetObjectRequest;
-import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
-import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -20,14 +17,13 @@ import java.nio.file.Path;
 public class S3Service {
 
     private final S3Client s3Client;
-    @Value("${spring.aws.bucketName}")
-    private String bucketName;
 
-    public String uploadFile(MultipartFile file, String folderName) throws IOException {
+    public void uploadFile(MultipartFile file, String folderName, String bucketName) throws IOException {
         String fileName = file.getOriginalFilename();
+        String encodedFileName = URLEncoder.encode(fileName, StandardCharsets.UTF_8);
         String key = folderName + "/" + fileName;
 
-        Path tempFile = Files.createTempFile("temp-", fileName);
+        Path tempFile = Files.createTempFile("temp-", encodedFileName);
         file.transferTo(tempFile.toFile());
 
         s3Client.putObject(PutObjectRequest.builder()
@@ -36,18 +32,5 @@ public class S3Service {
                 .build(), tempFile);
 
         Files.delete(tempFile);
-
-        return key;
     }
-
-    public InputStream downloadFile(String key) {
-        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
-                .bucket(bucketName)
-                .key(key)
-                .build();
-
-        // Download the file from S3 bucket
-        return s3Client.getObject(getObjectRequest);
-    }
-
 }
