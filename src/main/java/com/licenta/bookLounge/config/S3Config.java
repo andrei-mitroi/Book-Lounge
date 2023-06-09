909a9d52368777799bb422a1b0aeb81e382dbb67
mitroi.cristian.andrei@gmail.com <mitroi.cristian.andrei@gmail.com>
2023-06-09 16:50:53 +0300

    - last minute changes

--- src/main/java/com/licenta/bookLounge/config/S3Config.java
+++ src/main/java/com/licenta/bookLounge/config/S3Config.java
@@ -3,7 +3,9 @@ package com.licenta.bookLounge.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -13,11 +15,20 @@ public class S3Config {
 	@Value("${spring.aws.region}")
 	private String awsRegion;
 
+	@Value("${spring.aws.accessKey}")
+	private String awsAccessKeyId;
+
+	@Value("${spring.aws.secretKey}")
+	private String awsSecretKey;
+
 	@Bean
 	public S3Client s3Client() {
+		AwsCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+				AwsBasicCredentials.create(awsAccessKeyId, awsSecretKey));
+
 		return S3Client.builder()
 				.region(Region.of(awsRegion))
-				.credentialsProvider(DefaultCredentialsProvider.create())
+				.credentialsProvider(credentialsProvider)
 				.build();
 	}
 }
